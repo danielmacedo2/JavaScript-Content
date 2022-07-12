b53b00b9b272fef79889009bbc4f392c52f71846
Daniel Macedo <danielmsv2011@hotmail.com>
2022-07-12 07:33:49 -0300

    Initial commit

---
+++ callback_functions/script.js
@@ -0,0 +1,13 @@
+function sum(a, b) {
+    var operation = a + b;
+    console.log('A + B é igual a ' + operation)
+}
+
+function multiplication(a, b) {
+    var op = a * b;
+    console.log(`${a} X ${b} = ${op}`)
+}
+
+sum(2, 5);
+
+multiplication(5, 10)
