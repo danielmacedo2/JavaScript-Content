95a7562bea76a069376636ed75edac9285ff3127
Daniel Macedo <danielmsv2011@hotmail.com>
2022-07-12 08:56:51 -0300

    Initial commit

--- 03_async_await/script.js
+++ 03_async_await/script.js
@@ -1,1 +1,11 @@
 // Funções assíncronas
+function firstFunction()  {
+    return new Promise((resolve) => { // Arrow Function
+
+        setTimeout(() => { // as well
+            console.log("This is working")
+            resolve()
+        })
+    })
+
+}
