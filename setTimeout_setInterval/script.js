fdb500eb0275b764e2701fe3fa19a02893594416
Daniel Macedo <danielmsv2011@hotmail.com>
2022-07-12 08:47:57 -0300

    SetTimeout and setInterval

---
+++ setTimeout_setInterval/script.js
@@ -0,0 +1,13 @@
+// setTimeOut
+setTimeout(function() {
+
+    console.log("Testando o setTimeout")
+
+}, 3000)
+
+console.log("Antes do setTimeout")
+
+// setInterval
+setInterval(function () {
+    console.log("Testando o setInterval")
+}, 4000)
