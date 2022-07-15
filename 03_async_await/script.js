b67bfeb80c5ad5705b1ca5f5bfdb2030a7a794e0
Daniel Macedo <danielmsv2011@hotmail.com>
2022-07-15 07:42:25 -0300

    Funções Assincronas

--- 03_async_await/script.js
+++ 03_async_await/script.js
@@ -1,20 +1,43 @@
-// Funções assíncronas
-function firstFunction()  {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            console.log('Esperou isso aqui')
-            resolve()
-        }, 1000)
-    }) 
+// // Funções assíncronas
+// function firstFunction() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       console.log("Esperou isso aqui");
+//       resolve();
+//     }, 1000);
+//   });
+// }
 
+// async function secondFunction() {
+//   console.log("Iniciou");
+
+//   await firstFunction();
+
+//   console.log("Terminou");
+// }
+
+// secondFunction();
+
+// prático
+
+function getUser(id) {
+  return fetch(`https://reqres.in/api/users?id=${id}`)
+    .then((data) => data.json())
+    .catch((err) => console.log(err))
 }
 
-async function secondFunction() {
-    console.log('Iniciou')
 
-    await firstFunction()
+async function showUserName(id) {
 
-    console.log("Terminou")
+    try{
+
+    } catch(error) {
+        console.log(`Ocorreu um erro: ${error}`)
+    }
+
+    const user = await getUser(id)
+
+    console.log(`O nome do usuário é ${user.data.first_name}`)
 }
 
-secondFunction()
+showUserName(3)
